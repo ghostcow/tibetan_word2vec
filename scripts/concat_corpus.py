7732292ee0996b19aeed76f24b1185d965772aa3
Lior Uzan <sintzu@gmail.com>
2016-02-16 11:39:01 +0200

    changed some stuff

--- scripts/concat_corpus.py
+++ scripts/concat_corpus.py
@@ -1,8 +1,7 @@
 import os
-import re
 
 corpus = ''
-for root,_,files in os.walk('data/clean'):
+for root,_,files in os.walk('data/tokenize'):
     for textfile in files:
         with open(os.path.join(root,textfile)) as f:
             corpus += ' '
--- scripts/count_syllables.py
+++ scripts/count_syllables.py
@@ -1,7 +1,7 @@
 import os
 
 corpus = set()
-for root,_,files in os.walk('data/clean'):
+for root,_,files in os.walk('data/tokenize'):
     for textfile in files:
         with open(os.path.join(root,textfile)) as f:
             corpus |= set(f.read().split())
